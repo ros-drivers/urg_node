6a5460a70f7297124922f04f2f33731b152a00ca
Richard <richardw347@gmail.com>
2023-03-17 13:35:40 +0000

    increased delay in diagnostics thread to slow down publish rate

--- src/urg_node.cpp
+++ src/urg_node.cpp
@@ -340,7 +340,7 @@ void UrgNode::updateDiagnostics()
 {
   while (!close_diagnostics_) {
     diagnostic_updater_.force_update();
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 }
 
