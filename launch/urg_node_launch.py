1837c28047438eb80f04c228b8cb70dd524cb945
Patrick Jakubowski <patrick.jakubowski@ubica-robotics.eu>
2024-12-10 09:10:09 +0100

    DEBIAN/control; urg_node_launch.py was not starting

--- launch/urg_node_launch.py
+++ launch/urg_node_launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
     launch_description.add_action(param_file_path)
 
     hokuyo_node = Node(
-        package='urg_node', node_executable='urg_node', output='screen',
+        package='urg_node', executable='urg_node_driver', output='screen',
         parameters=[LaunchConfiguration('param')]
         )
 
